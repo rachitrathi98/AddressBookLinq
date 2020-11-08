9c33a3baf9dfd05e6861811d6e342454e890e942
Rachit Rathi <rachitrathi98@gmail.com>
2020-11-08 17:36:53 +0530

    [RAchit] Add. Table Commit

--- AddressBookLINQ/Program.cs
+++ AddressBookLINQ/Program.cs
@@ -6,6 +6,16 @@ namespace AddressBookLINQ
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Address Book LINQ!");
+            Console.WriteLine();
+            Console.WriteLine("Created DataTable for AddressBook");
+            Mgmt.CreateTable();
+            Console.WriteLine();
+            Console.WriteLine("UC3 Insert Row into DataTable AddressBook");
+            Mgmt.InsertRowIntoTable();
+            Console.WriteLine();
+            Console.WriteLine("UC4 Edit Record By Name into DataTable AddressBook");
+            Mgmt.EditContactByName();
 
         }
     }
